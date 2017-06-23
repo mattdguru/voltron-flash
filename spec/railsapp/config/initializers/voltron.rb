6233173b6be33b6c4fc313b92fc80ca16db3aa72
Eric Hainer <eric.hainer@gmail.com>
2017-06-23 13:40:17 -0600

    [CHG] Remove log level from config

--- spec/railsapp/config/initializers/voltron.rb
+++ spec/railsapp/config/initializers/voltron.rb
@@ -5,8 +5,6 @@ Voltron.setup do |config|
   # Whether to enable debug output in the browser console and terminal window
   config.debug = true
 
-  config.log_level = :debug
-
   # The base url of the site. Used by various voltron-* gems to correctly build urls
   # config.base_url = "http://localhost:3000"
 
